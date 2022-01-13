acd893381b93bcbb7d46930ffbc8bd97c1c3fdab
Matthias Märsch <you@example.com>
2022-01-13 16:37:47 +0100

    contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatenschutzComponent } from './datenschutz/datenschutz.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  { path: 'impressum', component: ImpressumComponent},
+  { path: 'datenschutz', component: DatenschutzComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
